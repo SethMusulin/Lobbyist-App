972e49f8f9dfd6ed3f8ffff79c4f2d93b896327c
Seth Musulin <sethmusulin@gmail.com>
2014-04-26 13:07:13 -0600

    Additional fix for background image bug

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+gem 'rails_12factor', '~> 0.0.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.0'
 # Use postgresql as the database for Active Record
@@ -31,7 +31,7 @@ group :test, :development do
   gem 'capybara', '~> 2.2.1'
 end
 
-gem 'rails_12factor', '~> 0.0.2'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -2,11 +2,14 @@ require 'spec_helper'
 
 feature "Homepage" do
   scenario "Shows welcome message" do
-  visit '/'
+    visit '/'
 
-  expect(page).to have_content "About"
-  click_link "About"
-  expect(page).to have_content "Welcome"
+    expect(page).to have_content "About"
+    click_link "About"
+    expect(page).to have_content "Welcome"
+    click_link "Home"
+    expect(page).to have_content "About"
+
 
   end
 end
