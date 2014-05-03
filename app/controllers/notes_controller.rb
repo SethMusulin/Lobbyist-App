7004d98552336934a682733db53171fdf517039b
Seth Musulin <sethmusulin@gmail.com>
2014-05-02 19:09:11 -0600

    User can edit and delete notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -22,6 +22,26 @@ class NotesController < ApplicationController
     @note = Note.find(params[:id])
   end
 
+  def edit
+    @note       = Note.find(params[:id])
+
+  end
+
+  def update
+    @note       = Note.find(params[:id])
+    @note.title = params[:note][:title]
+        @note.tag = params[:note][:tag]
+        @note.note = params[:note][:note]
+        @note.save
+    redirect_to "/notes/#{@note.id}"
+
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
+
 end
 
 
--- spec/features/notes_spec.rb
+++ spec/features/notes_spec.rb
@@ -13,6 +13,16 @@ feature "Note Pad" do
     click_link "Note Pad"
     click_link "test"
     expect(page).to have_content "This is a sweet test entry"
+    click_link "Edit Note"
+    fill_in "Tag", with: "edit test"
+    fill_in "Title", with: "edit test"
+    fill_in "Note", with: "This is a sweet test entry, and its edited counterpart"
+    click_on "Update Note"
+    expect(page).to have_content "edit test"
+    expect(page).to have_content "edit test"
+    expect(page).to have_content "This is a sweet test entry, and its edited counterpart"
+    click_on "Delete"
+    expect(page).to_not have_content "edit test"
   end
 end
 
