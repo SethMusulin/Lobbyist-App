4784d14340d5f9a08063577ded934abcfebfad5f
Seth Musulin <sethmusulin@gmail.com>
2014-05-25 14:46:52 -0600

    changing default mail address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "https://shrouded-garden-8917"
+  default from: "notifications@g2-social-network.com"
 
   def welcome_email(user)
     @user = user
