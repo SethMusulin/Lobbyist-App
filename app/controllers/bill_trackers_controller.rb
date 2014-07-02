b46995953dcf9ead648f17bcb6a7f7dcb321674a
Seth Musulin <sethmusulin@gmail.com>
2014-07-02 09:10:27 -0600

    adding edge case handling for user inputs

--- app/controllers/bill_trackers_controller.rb
+++ app/controllers/bill_trackers_controller.rb
@@ -5,8 +5,8 @@ class BillTrackersController < SignedinController
   def show
     @note = Note.new
     if params[:state] && params[:bill]
-      @state = params[:state]
-      @bill = params[:bill].gsub(/\d\d-/i, "").delete(' ').delete('-')
+      @state = params[:state].upcase
+      @bill = params[:bill].gsub(/\d\d-/i, "").delete(' ').delete('-').upcase
       @year = params[:year]
       @search = LegiScanApi.new(ENV['LEGISCAN'], @state, @bill, @year)
       @bill_results = @search.retrieve_bill_info
