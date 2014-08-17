6d96b531a7163a08595fb9ff84fc9f1071a93467
Seth Musulin <sethmusulin@gmail.com>
2014-08-17 01:24:06 -0600

    evernote

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@
 #   provider :developer unless Rails.env.production?
 #   provider :evernote, ENV['EN_CONSUMER_KEY'], ENV['EN_CONSUMER_SECRET']
 # end
-
+# #
 config = YAML.load(ERB.new(File.read('config/evernote.yml')).result)[Rails.env]
 site = config['sandbox'] ? 'https://sandbox.evernote.com' : 'https://www.evernote.com'
 
