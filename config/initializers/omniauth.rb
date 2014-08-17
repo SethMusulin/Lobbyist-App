0736422bce1e5488f659116be6e924439eeca658
Seth Musulin <sethmusulin@gmail.com>
2014-08-17 02:53:53 -0600

    eve

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,14 @@
 #   provider :developer unless Rails.env.production?
 #   provider :evernote, ENV['EN_CONSUMER_KEY'], ENV['EN_CONSUMER_SECRET']
 # end
+#
+# config = YAML.load(ERB.new(File.read('config/evernote.yml')).result)[Rails.env]
+# site = config['sandbox'] ? 'https://sandbox.evernote.com' : 'https://www.evernote.com'
+#
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :evernote, config['consumer_key'], config['consumer_secret'], :client_options => {:site => site}
+# end
+
 
 config = YAML.load(ERB.new(File.read('config/evernote.yml')).result)[Rails.env]
 site = config['sandbox'] ? 'https://sandbox.evernote.com' : 'https://www.evernote.com'
