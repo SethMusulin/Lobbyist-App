243422aae9e166ed838522866e4d266b6d0ad0bb
Seth Musulin <sethmusulin@gmail.com>
2014-08-18 17:50:46 -0600

    adding test for note title as required by evernote api

--- spec/models/notes_model_spec.rb
+++ spec/models/notes_model_spec.rb
@@ -2,8 +2,17 @@ require 'spec_helper'
 
 describe 'Notes' do
   it "must have actual text to be valid" do
-    note = Note.create!(:note => "this is a test note", :title =>"Test")
+    note = Note.new(:note => "this is a test note", :title =>"Test")
+    expect(note).to be_valid
     note.note = ""
+    expect(note).to_not be_valid
+
+  end
+  it "must have actual title to be valid" do
+    note = Note.create!(:note => "this is a test note", :title =>"Test")
+    expect(note).to be_valid
+
+    note.title = ""
     expect(note).to_not be_valid
 
   end
