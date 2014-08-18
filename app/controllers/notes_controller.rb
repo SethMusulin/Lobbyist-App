8daa83fbc8d0429e43bcdcfc035050b3827f52cc
Seth Musulin <sethmusulin@gmail.com>
2014-08-18 08:33:11 -0600

    Amending link to Evernote until bug is fixed

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,7 +2,7 @@ class NotesController < SignedinController
 
   def index
     @notes=current_user.notes.paginate(page: params[:page], per_page: 10).order('created_at DESC')
-  
+
   end
 
   def new
